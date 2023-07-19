b20e1d9dd94747db418d80ef7c1cbf0597b77046
HiranyaRanawaka99 <hiranya.ranawaka99@gmail.com>
2023-07-19 17:36:04 +0300

    add: step 5

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio,
 1. il prezzo del biglietto è definito in base ai km (0.21 € al km)
 2. va applicato uno sconto del 20% per i minorenni
 3. va applicato uno sconto del 40% per gli over 65.
-L'output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo). Questo richiederà un minimo di ricerca.
+L'output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo).
 
 ## Istruzioni
 
@@ -16,3 +16,4 @@ L'output del prezzo finale va messo fuori in forma umana (con massimo due decima
 2. Definire prezzo/KM
 3. Definire sconto del 20% per < 18
 4. Definire sconto del 40% per > 65
+5. Rendere il prezzo finale con soli due decimali.
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,26 @@
 const Km = prompt('Quanti KM devi percorrere?');
-console.log ('KM da percorrere: ' + Km);
+//console.log ('KM da percorrere: ' + Km);
 
 const eta = prompt('Quanti anni hai?');
-console.log ('Età: ' + eta);
+//console.log ('Età: ' + eta);
 
 const prezzoKm = Km * 0.21;
-console.log ('Prezzo per KM: ' + prezzoKm);
+//console.log ('Prezzo per KM: ' + prezzoKm);
+
+
+/*Stampiamo in HTML */
+
+document.getElementById('km').innerHTML = 'KM da percorrere: ' + Km;
+
+document.getElementById('eta').innerHTML = 'La tua età: ' + eta;
+
 
 if (eta < 18) {
-    alert ('Hai diritto a uno sconto del 20%. Il prezzo totale del tuo biglietto è: ' + (prezzoKm -((prezzoKm*20)/100)) + ' €')
+    document.getElementById('sconto').innerHTML = 'Hai diritto a uno sconto del 20%. Il prezzo totale del tuo biglietto è: ' + (prezzoKm -((prezzoKm*20)/100)).toFixed(2) + ' €'
 } else if (eta > 65) {
-    alert ('Hai diritto a uno sconto del 40%. Il prezzo totale del tuo biglietto è: ' + (prezzoKm -((prezzoKm*40)/100)) + ' €')
+    document.getElementById('sconto').innerHTML = 'Hai diritto a uno sconto del 40%. Il prezzo totale del tuo biglietto è: ' + (prezzoKm -((prezzoKm*40)/100)).toFixed(2) + ' €'
+} else {
+    document.getElementById('sconto').innerHTML = 'Il prezzo totale del tuo biglietto è: ' + prezzoKm.toFixed(2) + '€'
 }
+
+
